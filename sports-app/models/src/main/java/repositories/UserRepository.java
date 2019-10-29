d2fc912d8cc53dfea452430d31ecbd0e790570cc
skarning <sivertskarning@hotmail.com>
2019-10-29 13:31:33 +0100

    Add repository for users to abstract data access

---
+++ sports-app/models/src/main/java/repositories/UserRepository.java
@@ -0,0 +1,26 @@
+package repositories;
+
+import models.User;
+import java.util.List;
+
+public class UserRepository implements IRepository<User>{
+    @Override
+    public User getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public void add(User user) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+}
