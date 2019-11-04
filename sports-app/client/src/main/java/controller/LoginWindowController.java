004c34cf0f6af8fad1908b428da08ba39c2b6177
Owlar <oscarlr@hiof.no>
2019-11-04 23:09:01 +0100

    1. Move MainJavaFX to package "main" because can't make instance of MainJavaFX when it's located in default package & 2. Add login controller with interface plus stuff

--- sports-app/client/src/main/java/controller/LoginWindowController.java
+++ sports-app/client/src/main/java/controller/LoginWindowController.java
@@ -1,4 +1,33 @@
 package controller;
 
-public class LoginWindowController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import main.MainJavaFX;
+
+public class LoginWindowController implements ILoginWindowController {
+
+    @FXML
+    private Button loginButtonClub, loginButtonAdmin;
+
+    private MainJavaFX mainJavaFx = new MainJavaFX();
+
+    public void initialize() {
+        loginButtonClub = new Button("Club");
+        loginButtonAdmin = new Button("Admin");
+    }
+
+    @FXML
+    @Override
+    public void goToClubWindowWhenClicked() {
+        mainJavaFx = new MainJavaFX();
+        mainJavaFx.clubWindow(new Stage());
+    }
+
+    @FXML
+    @Override
+    public void goToAdminWindowWhenClicked() {
+        mainJavaFx = new MainJavaFX();
+        mainJavaFx.adminWindow(new Stage());
+    }
 }
---
+++ sports-app/client/src/main/java/main/MainJavaFX.java
@@ -0,0 +1,83 @@
+package main;
+
+import controller.LoginWindowController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainJavaFX extends Application {
+
+    private Stage primaryStage;
+    private Stage loginWindow;
+    private static MainJavaFX application;
+
+    public Stage getLoginWindow() {
+        return loginWindow;
+    }
+
+    public MainJavaFX() {
+        application = this;
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.primaryStage = stage;
+        loginWindow();
+    }
+
+    private void loginWindow() throws IOException {
+        try {
+            /*
+            String javaVersion = System.getProperty("java.version");
+            String javafxVersion = System.getProperty("javafx.version");
+            */
+
+            Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/LoginWindow.fxml"));
+            primaryStage.setTitle("Please login");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
+        } catch (IllegalStateException ise) {
+            ise.printStackTrace();
+        }
+    }
+
+    public void clubWindow(Stage clubStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/ClubWindow.fxml"));
+            clubStage.setTitle("Welcome Club");
+            clubStage.setScene(new Scene(root));
+            clubStage.initModality(Modality.APPLICATION_MODAL);
+            clubStage.initOwner(getLoginWindow());
+            clubStage.show();
+        } catch (IllegalStateException ise) {
+            ise.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+    public void adminWindow(Stage adminStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/AdminWindow.fxml"));
+            adminStage.setTitle("Welcome Admin");
+            adminStage.setScene(new Scene(root));
+            adminStage.initModality(Modality.APPLICATION_MODAL);
+            adminStage.initOwner(getLoginWindow());
+            adminStage.show();
+        } catch (IllegalStateException ise) {
+            ise.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
