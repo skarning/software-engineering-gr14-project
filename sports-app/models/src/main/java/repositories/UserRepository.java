c27d3e89fe6628c105de5982a65a7fbb69ee7867
skarning <sivertskarning@hotmail.com>
2019-11-07 19:49:58 +0100

    Add User controllor and implementedepository for users

--- sports-app/models/src/main/java/repositories/UserRepository.java
+++ sports-app/models/src/main/java/repositories/UserRepository.java
@@ -3,9 +3,13 @@ package repositories;
 import models.User;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class UserRepository implements IRepository<User>{
+    ArrayList<User> users;
+
+    public UserRepository() {
+        this.users = new ArrayList<User>();
+    }
 
     @Override
     public User getById(int id) {
@@ -13,17 +17,17 @@ public class UserRepository implements IRepository<User>{
     }
 
     @Override
-    public List<User> getAll() {
-        return null;
+    public ArrayList<User> getAll() {
+        return users;
     }
 
     @Override
     public void add(User user) {
-
+        users.add(user);
     }
 
     @Override
     public void delete(User user) {
-
+        users.remove(user);
     }
 }
