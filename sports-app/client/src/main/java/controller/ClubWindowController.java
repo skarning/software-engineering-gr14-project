b10bb3ded27a3e2d525f34403d489fe8e7664930
skarning <sivertskarning@hotmail.com>
2019-11-14 21:20:52 +0100

    Add GUI for create new event

--- sports-app/client/src/main/java/controller/ClubWindowController.java
+++ sports-app/client/src/main/java/controller/ClubWindowController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import main.MainJavaFX;
+
 public class ClubWindowController {
+    @FXML
+    private Button CreateEventButton = new Button();
+    MainJavaFX mainJavaFX = new MainJavaFX();
+
+    @FXML
+    public void OpenCreateNewEventWindow() {
+        mainJavaFX.eventWindow(new Stage());
+    }
 }
--- sports-app/client/src/main/java/main/IMainJavaFX.java
+++ sports-app/client/src/main/java/main/IMainJavaFX.java
@@ -6,4 +6,5 @@ public interface IMainJavaFX {
     void loginWindow();
     void clubWindow(Stage stage);
     void adminWindow(Stage stage);
+    void eventWindow(Stage stage);
 }
