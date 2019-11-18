68ff7f81e12b9db3d5fca4125eee68b799ec9cd9
skarning <sivertskarning@hotmail.com>
2019-11-18 09:33:41 +0100

    Add Mockup database to move data around from window to windowd without losing data

---
+++ sports-app/models/src/main/java/repositories/MockupDatabase.java
@@ -0,0 +1,12 @@
+package repositories;
+
+import models.Event;
+import models.User;
+
+import java.util.ArrayList;
+
+public class MockupDatabase {
+    public static ArrayList<User> users = new ArrayList<User>();
+
+    public static ArrayList<Event> events = new ArrayList<Event>();
+}
